0e16f35a33609a2a9c181c21d51e5e4962f84ac0
Lee Bergstrand <leemacboy@gmail.com>
2017-05-02 13:09:54 -0700

    Update indenting to be spaces according to pep8.

--- Get16S.py
+++ Get16S.py
@@ -23,73 +23,77 @@
 # ===========================================================================================================
 # Imports:
 
-import sys
 import csv
+import sys
+
 from SeqExtract import entrezEmail
+from SeqExtract import extractContigs
 from SeqExtract import getSeqRecords
 from SeqExtract import isSSProject
-from SeqExtract import extractContigs
+
+
 # ===========================================================================================================
 # Functions:
 
 
 # 1: Checks if in proper number of arguments are passed gives instructions on proper use.
 def argsCheck(numArgs):
-	if len(sys.argv) < numArgs or len(sys.argv) > numArgs:
-		print("Coding Sequence Downloader")
-		print("By Lee Bergstrand\n")
-		print("Usage: " + sys.argv[0] + " <sequences.txt> [email@mail.com]")
-		print("Examples: " + sys.argv[0] + " mySeqs.txt JBro@YOLO.com\n")
-		print("Please Note:")
-		print("Before using this script to access the NCBI's online resources please read the NCBI's")
-		print("Entrez User Requirements. If the NCBI finds you are abusing their systems, they can")
-		print("and will ban your access! Use the optional email parameter so the NCBI can contact")
-		print("you if there is a problem.")
-		sys.exit(1)  # Aborts program. (exit(1) indicates that an error occurred)
+    if len(sys.argv) < numArgs or len(sys.argv) > numArgs:
+        print("Coding Sequence Downloader")
+        print("By Lee Bergstrand\n")
+        print("Usage: " + sys.argv[0] + " <sequences.txt> [email@mail.com]")
+        print("Examples: " + sys.argv[0] + " mySeqs.txt JBro@YOLO.com\n")
+        print("Please Note:")
+        print("Before using this script to access the NCBI's online resources please read the NCBI's")
+        print("Entrez User Requirements. If the NCBI finds you are abusing their systems, they can")
+        print("and will ban your access! Use the optional email parameter so the NCBI can contact")
+        print("you if there is a problem.")
+        sys.exit(1)  # Aborts program. (exit(1) indicates that an error occurred)
 
 
 # ---------------------------------------------------------------------------------------------------------
 # 2: Gets reverse complement of DNA string.
 def reverseCompliment(sequence):
-	sequence.upper()
+    sequence.upper()
 
-	basecomplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-	letters = list(sequence)
-	letters = [basecomplement[base] for base in letters]
-	sequence = "".join(letters)
+    basecomplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    letters = list(sequence)
+    letters = [basecomplement[base] for base in letters]
+    sequence = "".join(letters)
 
-	sequence = sequence[::-1]  # Reverses sequence using pythons string slice syntax.
-	return sequence
+    sequence = sequence[::-1]  # Reverses sequence using pythons string slice syntax.
+    return sequence
 
 
 # ---------------------------------------------------------------------------------------------------------
 # 3: Gets 16S DNA as a fasta.
 def extract16sFasta(organismID, feature, record):
-	start = feature.location.nofuzzy_start
-	end = feature.location.nofuzzy_end
-	strand = feature.location.strand
-	sequence = str(record.seq[start:end])  # Extracts subsequence from the genome according to location of the feature.
+    start = feature.location.nofuzzy_start
+    end = feature.location.nofuzzy_end
+    strand = feature.location.strand
+    sequence = str(record.seq[start:end])  # Extracts subsequence from the genome according to location of the feature.
 
-	if strand == -1:  # Converts subsequence to reverse complement if on negative strand.
-		sequence = reverseCompliment(sequence)
+    if strand == -1:  # Converts subsequence to reverse complement if on negative strand.
+        sequence = reverseCompliment(sequence)
 
-	fasta = ">%s\n%s" % (organismID, sequence)
-	return fasta
+    fasta = ">%s\n%s" % (organismID, sequence)
+    return fasta
 
 
 # ---------------------------------------------------------------------------------------------------------
 # 4: Gets a list of 16s FASTAs from a genome.
 def get16sFasta(sequenceID, record):
-	FASTAS = []
-	organismID = sequenceID
-	features = record.features
-	for feature in features:
-		if feature.type == "rRNA":
-			if "16S" in feature.qualifiers["product"][0]:
-				fasta = extract16sFasta(organismID, feature, record)
-				if 1000 < len(fasta) < 2000:  # Removes partial sequences and really large sequence do to genbank.
-					FASTAS.append(fasta)
-	return FASTAS
+    FASTAS = []
+    organismID = sequenceID
+    features = record.features
+    for feature in features:
+        if feature.type == "rRNA":
+            if "16S" in feature.qualifiers["product"][0]:
+                fasta = extract16sFasta(organismID, feature, record)
+                if 1000 < len(fasta) < 2000:  # Removes partial sequences and really large sequence do to genbank.
+                    FASTAS.append(fasta)
+    return FASTAS
+
 
 # ===========================================================================================================
 # Main program code:
@@ -105,16 +109,16 @@ inFile = sys.argv[1]
 
 # File extension check
 if not inFile.endswith(".txt"):
-	print("[Warning] " + inFile + " may not be a txt file!")
+    print("[Warning] " + inFile + " may not be a txt file!")
 
 # Reads sequence file list and stores it as a string object. Safely closes file.try:
 try:
-	with open(inFile, "r") as newFile:
-		sequences = newFile.read()
-		newFile.close()
+    with open(inFile, "r") as newFile:
+        sequences = newFile.read()
+        newFile.close()
 except IOError:
-	print("Failed to open " + inFile)
-	sys.exit(1)
+    print("Failed to open " + inFile)
+    sys.exit(1)
 
 seqList = sequences.splitlines()  # Splits string into a list. Each element is a single line from the string.
 
@@ -123,53 +127,53 @@ print()
 print(sequences + "\n")
 
 seqRecords = getSeqRecords(
-	seqList)  # Acquires list of sequence record objects from NCBI using the sequence list as reference.
+    seqList)  # Acquires list of sequence record objects from NCBI using the sequence list as reference.
 
 No16sGenomes = []
 SixTeens = []
 for sequence in seqRecords:
-	sequenceID = sequence.id
-	No16s = True
-	if "plasmid" in sequence.description.lower():  # If sequence is from a plasmid skip the iteration.
-		continue
-	if isSSProject(sequence):  # If accession is a WGSS project...
-		contigList = extractContigs(sequence.id)  # Extract all contig accessions.
-		contigRecords = getSeqRecords(contigList)  # Extract sequence record object for each contig.
-		for contig in contigRecords:
-			fasta = get16sFasta(sequenceID, contig)  # Builds list fasta files.
-			if fasta:  # If 16S is found.
-				No16s = False
-				SixTeens.append(fasta[0])
-				break  # If 16S is found in one contig break out and skip all the other contigs
-	else:  # If accession is a regular genome...
-		fasta = get16sFasta(sequenceID, sequence)  # Builds list fasta files.
-		if fasta:
-			No16s = False
-			SixTeens.append(fasta[0])
-	if No16s:  # If not 16S is found add genome to the no 16s found list.
-		No16sGenomeInfo = [sequenceID, sequence.annotations["organism"]]
-		No16sGenomes.append(No16sGenomeInfo)
+    sequenceID = sequence.id
+    No16s = True
+    if "plasmid" in sequence.description.lower():  # If sequence is from a plasmid skip the iteration.
+        continue
+    if isSSProject(sequence):  # If accession is a WGSS project...
+        contigList = extractContigs(sequence.id)  # Extract all contig accessions.
+        contigRecords = getSeqRecords(contigList)  # Extract sequence record object for each contig.
+        for contig in contigRecords:
+            fasta = get16sFasta(sequenceID, contig)  # Builds list fasta files.
+            if fasta:  # If 16S is found.
+                No16s = False
+                SixTeens.append(fasta[0])
+                break  # If 16S is found in one contig break out and skip all the other contigs
+    else:  # If accession is a regular genome...
+        fasta = get16sFasta(sequenceID, sequence)  # Builds list fasta files.
+        if fasta:
+            No16s = False
+            SixTeens.append(fasta[0])
+    if No16s:  # If not 16S is found add genome to the no 16s found list.
+        No16sGenomeInfo = [sequenceID, sequence.annotations["organism"]]
+        No16sGenomes.append(No16sGenomeInfo)
 OutSixTeens = "\n".join(SixTeens)
 
 try:
-	# Attempted to create to output files.
-	outFile = "SixTeenSSFromGenbank.fna"
-	outCSVFile = "NoSixTeenGenomes.csv"
+    # Attempted to create to output files.
+    outFile = "SixTeenSSFromGenbank.fna"
+    outCSVFile = "NoSixTeenGenomes.csv"
 
-	print("Writing " + outFile + " to file...")
-	writeFile = open(outFile, "w")
-	writeFile.write(OutSixTeens)
-	writeFile.close()
+    print("Writing " + outFile + " to file...")
+    writeFile = open(outFile, "w")
+    writeFile.write(OutSixTeens)
+    writeFile.close()
 
-	if No16sGenomes:
-		print("Writing " + outCSVFile + " to file...")
-		csvFile = open(outCSVFile, "w")
-		CSVWriter = csv.writer(csvFile)
-		for genome in No16sGenomes:
-			CSVWriter.writerow(genome)
-		csvFile.close()
+    if No16sGenomes:
+        print("Writing " + outCSVFile + " to file...")
+        csvFile = open(outCSVFile, "w")
+        CSVWriter = csv.writer(csvFile)
+        for genome in No16sGenomes:
+            CSVWriter.writerow(genome)
+        csvFile.close()
 except IOError:
-	print("Failed to create " + outFile)
-	sys.exit(1)
+    print("Failed to create " + outFile)
+    sys.exit(1)
 
 print("Done!")
--- GetNucleotide.py
+++ GetNucleotide.py
@@ -20,80 +20,85 @@
 # ----------------------------------------------------------------------------------------
 # ===========================================================================================================
 # Imports:
-	
+
 import sys
+
 from SeqExtract import entrezEmail
+from SeqExtract import extractContigs
 from SeqExtract import getSeqRecords
 from SeqExtract import isSSProject
-from SeqExtract import extractContigs
+
+
 # ===========================================================================================================
 # Functions:
 
 
 # 1: Checks if in proper number of arguments are passed gives instructions on proper use.
 def argsCheck(numArgs):
-	if len(sys.argv) < numArgs or len(sys.argv) > numArgs:
-		print("Sequence Downloader")
-		print("By Lee Bergstrand\n")
-		print("Usage: " + sys.argv[0] + " <sequences.txt> [email@mail.com]")
-		print("Examples: " + sys.argv[0] + " mySeqs.txt JBro@YOLO.com\n")
-		print("Please Note:")
-		print("Before using this script to access the NCBI's online resources please read the NCBI's") 
-		print("Entrez User Requirements. If the NCBI finds you are abusing their systems, they can") 
-		print("and will ban your access! Use the optional email parameter so the NCBI can contact") 
-		print("you if there is a problem.")
-		sys.exit(1) # Aborts program. (exit(1) indicates that an error occurred)
+    if len(sys.argv) < numArgs or len(sys.argv) > numArgs:
+        print("Sequence Downloader")
+        print("By Lee Bergstrand\n")
+        print("Usage: " + sys.argv[0] + " <sequences.txt> [email@mail.com]")
+        print("Examples: " + sys.argv[0] + " mySeqs.txt JBro@YOLO.com\n")
+        print("Please Note:")
+        print("Before using this script to access the NCBI's online resources please read the NCBI's")
+        print("Entrez User Requirements. If the NCBI finds you are abusing their systems, they can")
+        print("and will ban your access! Use the optional email parameter so the NCBI can contact")
+        print("you if there is a problem.")
+        sys.exit(1)  # Aborts program. (exit(1) indicates that an error occurred)
+
+
 # ===========================================================================================================
 # Main program code:
-	
+
 # House keeping...
-argsCheck(3) # Checks if the number of arguments are correct.
-entrezEmail(sys.argv[2]) # Sets up arguments email require for genbank file extraction.
-	
+argsCheck(3)  # Checks if the number of arguments are correct.
+entrezEmail(sys.argv[2])  # Sets up arguments email require for genbank file extraction.
+
 # Stores file one for input checking.
 print(">> Opening sequence list...")
-inFile  = sys.argv[1]
+inFile = sys.argv[1]
 
 # File extension check
 if not inFile.endswith(".txt"):
-	print("[Warning] " + inFile + " may not be a txt file!")
+    print("[Warning] " + inFile + " may not be a txt file!")
 
 # Reads sequence file list and stores it as a string object. Safely closes file:
-try:	
-	with open(inFile,"r") as newFile:
-		sequences = newFile.read()
-		newFile.close()
+try:
+    with open(inFile, "r") as newFile:
+        sequences = newFile.read()
+        newFile.close()
 except IOError:
-	print("Failed to open " + inFile)
-	sys.exit(1)
+    print("Failed to open " + inFile)
+    sys.exit(1)
 
 seqList = sequences.splitlines()  # Splits string into a list. Each element is a single line from the string.
 
 print("You have listed", len(seqList), "sequences. They are:")
 print(sequences + "\n\n")
-	
+
 seqRecords = getSeqRecords(seqList)  # Gets sequence record objects from NCBI using the sequence list as reference.
 
 for sequence in seqRecords:
-	outFile = sequence.id + ".fna"
-	try:
-		# Attempted to create to output file.
-		writeFile = open(outFile, "w") 	
-		print("Writing " + outFile + " to file...")
-		
-		# Checks if the accession leads to a WGSS project. 
-		# If accession is a WGSS project... 
-		if isSSProject(sequence):
-			contigList = extractContigs(sequence.id)  # Extract all contig accessions.
-			contigRecords = getSeqRecords(contigList)  # Extract sequence record object for each contig.
-			for contig in contigRecords:
-				writeFile.write(contig.format("fasta"))  # Write each contig to the same file in fasta format.
-		# If accession is a regular genome... 
-		else:	
-			writeFile.write(sequence.format("fasta"))  # Write genome as fasta to file
-		writeFile.close()
-	except IOError:
-		print("Failed to create " + outFile)
-		sys.exit(1)	
-	
+    outFile = sequence.id + ".fna"
+    try:
+        # Attempted to create to output file.
+        writeFile = open(outFile, "w")
+        print("Writing " + outFile + " to file...")
+
+        # Checks if the accession leads to a WGSS project.
+        # If accession is a WGSS project...
+        if isSSProject(sequence):
+            contigList = extractContigs(sequence.id)  # Extract all contig accessions.
+            contigRecords = getSeqRecords(contigList)  # Extract sequence record object for each contig.
+            for contig in contigRecords:
+                writeFile.write(contig.format("fasta"))  # Write each contig to the same file in fasta format.
+        # If accession is a regular genome...
+        else:
+            writeFile.write(sequence.format("fasta"))  # Write genome as fasta to file
+        writeFile.close()
+    except IOError:
+        print("Failed to create " + outFile)
+        sys.exit(1)
+
 print("Done!")
